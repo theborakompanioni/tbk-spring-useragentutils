3e4eee5af8773cab71380e2fcfc61a3944697100
theborakompanioni <theborakompanioni+github@gmail.com>
2015-07-06 22:50:01 +0200

    adapt UserAgentUtils
    make final; hide constructor; package private static attribute for "current browser" key

--- src/main/java/com/github/theborakompanioni/spring/useragentutils/UserAgentUtils.java
+++ src/main/java/com/github/theborakompanioni/spring/useragentutils/UserAgentUtils.java
@@ -5,17 +5,18 @@ import org.springframework.web.context.request.RequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class UserAgentUtils {
-    public static final String CURRENT_USERAGENT_ATTRIBUTE = "currentUserAgent";
+public final class UserAgentUtils {
+    static final String CURRENT_USERAGENT_ATTRIBUTE = "currentUserAgent";
 
-    public UserAgentUtils() {
+    private UserAgentUtils() {
+        throw new UnsupportedOperationException();
     }
 
     public static UserAgent getCurrentUserAgent(HttpServletRequest request) {
-        return (UserAgent)request.getAttribute(CURRENT_USERAGENT_ATTRIBUTE);
+        return (UserAgent) request.getAttribute(CURRENT_USERAGENT_ATTRIBUTE);
     }
 
     public static UserAgent getCurrentUserAgent(RequestAttributes attributes) {
-        return (UserAgent)attributes.getAttribute(CURRENT_USERAGENT_ATTRIBUTE, 0);
+        return (UserAgent) attributes.getAttribute(CURRENT_USERAGENT_ATTRIBUTE, 0);
     }
 }
